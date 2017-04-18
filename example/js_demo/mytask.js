def179d4d539ade71cf229e0b7a004ad8ee02cbf
Diogoxiang <a233894432@gmail.com>
2017-04-18 20:10:49 +0800

    测试.worker

--- example/js_demo/mytask.js
+++ example/js_demo/mytask.js
@@ -3,3 +3,8 @@ postMessage("I\'m working before postMessage(\'ali\').");
 onmessage = function(oEvent) {
     postMessage("Hi " + oEvent.data);
 };
+
+
+setTimeout(function() {
+    postMessage("I\'ss");
+}, 2000)
